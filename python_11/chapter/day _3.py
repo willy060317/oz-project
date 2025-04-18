2baf50c9502020aca2bf9fa1f1b5c08b98c31ba8
willy060317 <willy031712345678@gmail.com>
2025-04-18 22:50:19 +0900

    python_11

--- python_11/chapter/day _3.py
+++ python_11/chapter/day _3.py
@@ -1,3 +1,18 @@
-str = "a b c"
-print(str.split())
-하지마
+# # # # # str = "a b c"
+# # # # # print(str.split()하지
+# # # # # input()
+# # # # # 안녕ㅏ세요
+# # # # print("python")
+# # # # print("python".find("p       ython"))
+# # # print("Hello".isupper())
+# # # print( "a" in "apple")
+# # my_string = "jarson"
+# # print(my_string[::-1])
+# pip3 install requests
+import requests
+
+keyword = input("검색할 키워드를 입력해주세요 :")
+url ="https://search.naver.com/search.naver?ssc=tab.blog.all&sm=tab_jum&query=" + keyword
+
+html = requests.get(url)
+print(html.text)
