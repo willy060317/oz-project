1f58a83c2340bc286d1886af23bb1dbcc728ce3e
willy060317 <willy031712345678@gmail.com>
2025-04-23 23:47:06 +0900

    python_11

--- python_11/chapter/day _3.py
+++ python_11/chapter/day _3.py
@@ -1,34 +1,68 @@
-# # # # # str = "a b c"
-# # # # # print(str.split()하지
-# # # # # input()
-# # # # # 안녕ㅏ세요
-# # # # print("python")
-# # # # print("python".find("p       ython"))
-# # # print("Hello".isupper())
-# # # print( "a" in "apple")
-# # my_string = "jarson"
-# # print(my_string[::-1])
-# pip3 install requests
-# import requests
+# # # # # # # # # # # str = "a b c"
+# # # # # # # # # # # print(str.split()하지
+# # # # # # # # # # # input()
+# # # # # # # # # # # 안녕ㅏ세요
+# # # # # # # # # # print("python")
+# # # # # # # # # # print("python".find("p       ython"))
+# # # # # # # # # print("Hello".isupper())
+# # # # # # # # # print( "a" in "apple")
+# # # # # # # # my_string = "jarson"
+# # # # # # # # print(my_string[::-1])
+# # # # # # # pip3 install requests
+# # # # # # # import requests
 
-# keyword = input("검색할 키워드를 입력해주세요 :")
-# url ="https://search.naver.com/search.naver?ssc=tab.blog.all&sm=tab_jum&query=" + keyword
+# # # # # # # keyword = input("검색할 키워드를 입력해주세요 :")
+# # # # # # # url ="https://search.naver.com/search.naver?ssc=tab.blog.all&sm=tab_jum&query=" + keyword
 
-# html = requests.get(url)
-# print(html.text)
-# i=0
-# while True:
-#     i += 1
-#     if i>9999999999999*9999999999999999999999999999:
-#         break
-# print(i)
-num = 7
-정답 = 0
-while num != 1:
-    if num%2==0:
-        num = num/2
-    else:
-        num=num*3+1
+# # # # # # # html = requests.get(url)
+# # # # # # # print(html.text)
+# # # # # # # i=0
+# # # # # # # while True:
+# # # # # # #     i += 1
+# # # # # # #     if i>9999999999999*9999999999999999999999999999:
+# # # # # # #         break
+# # # # # # # print(i)
+# # # # # # num = 7
+# # # # # # 정답 = 0
+# # # # # # while num != 1:
+# # # # # #     if num%2==0:
+# # # # # #         num = num/2
+# # # # # #     else:
+# # # # # #         num=num*3+1
     
-    정답+= 1
-print(정답)    
+# # # # # #     정답+= 1
+# # # # # # print(정답)    
+# # # # # print(dir(int))
+# # # # # class Person:
+# # # #      def __init__(self, name, age, address):
+# # # #          self.hello = "안녕하세요"
+# # # def function(x, y):
+# # #     z = x + y
+# # #     return z
+# # # print('옥수수와 수박의 수확량 :', function(10,2
+# # a=100
+# # def f():
+# #     global a
+# # #     a=a+1
+# # #     return a
+# # # # print(f()
+# # class Person:
+# #     def hello(self):
+# #         print('수강생 여러분')
+
+# # 호출=Person()
+# # # 호출.hello()
+# # a = int(10)
+# # print(a)
+# # b=list(range(10))
+# # print(b)
+# # class 
+# # print (dir(int))
+# class 클래스아름:
+class oz:
+    def __init__(self,name,age,coding_level):
+        self.name =name
+        self.age =age
+        self.coding_level=coding_level
+        coding = oz("김코", 30, "A")
+        print(oz)
